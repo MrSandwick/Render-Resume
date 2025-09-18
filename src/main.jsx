c446a475e3f9d663d56b391b6c0148b9bfd0181c
Baatyrbek <batrudinovshapiro@gmail.com>
2025-09-18 02:02:46 -0500

    added projects page

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { HashLink } from 'react-router-hash-link';
 import App from './App.jsx'
 import TechPage from './pages/techdetails.jsx'
+import ProjectsPage from './pages/projects.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -11,7 +12,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<App />} />
-				<Route path="/tech" element={<TechPage />} />
+				<Route path="/tech" element={<TechPage />} />\
+				<Route path="/projects" element={<ProjectsPage />} />
 			</Routes>
 		</BrowserRouter>
 	</React.StrictMode>
---
+++ src/pages/projects.jsx
@@ -0,0 +1,13 @@
+import Navbar from '../components/navbar.jsx'
+
+export default function ProjectsPage() {
+    return (
+        <div className="w-full min-h-screen bg-black text-whiter text-center">
+            <Navbar />
+            <h1 className="text-3xl font-bold">My Projects</h1>
+            <p className="opacity-80">
+                Lorem ipsum dolor sit amet.
+            </p>
+        </div>
+    )
+}
